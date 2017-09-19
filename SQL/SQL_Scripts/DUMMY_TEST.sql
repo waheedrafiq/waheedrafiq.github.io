e247b603439caf5f5351e23587bbe1c976976f49
waheedrafiq <waheedrafiq@outlook.com>
2017-09-19 10:55:42 +0100

    futher update on SQL and VB webpage

--- SQL/SQL_Scripts/DUMMY_TEST.sql
+++
@@ -1,9 +0,0 @@
-use dummy
-CREATE TABLE dummyTest
-(
-   dummyTest_ID int IDENTITY(1,1) PRIMARY KEY,
-   dum_Fname varchar(100),
-   dum_Dateorder datetime not null DEFAULT GETDATE()
-
-
-)-- experiment area 
--- SQL/SQL_Scripts/test_bed.sql
+++
@@ -1,6 +0,0 @@
-
-Use EPOSv2
-GO
-
-INSERT INTO tbl_Users (Users_Fname,Users_Sname,Users_Email,Users_Username,Users_ImagePath)
-VALUES ('BOB','JOES','BJ@EMAIL.COM','bj','/App_Data/Images/Users/bj.jpg')
